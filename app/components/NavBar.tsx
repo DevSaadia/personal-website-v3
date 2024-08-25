446bbcfeb0614ccf5833a1eeb37eaa0d297712a3
DevSaadia <saadia.shahid@torontomu.ca>
2024-08-25 01:52:54 -0400

    updated navbar ui

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,7 +2,11 @@ import Link from 'next/link'
 import path from 'path'
 import React from 'react'
 import NavLink from './NavLink'
-
+import { JetBrains_Mono } from '@next/font/google'
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
 const NavBar = () => {
   const navLinks = [
     {
@@ -17,9 +21,9 @@ const NavBar = () => {
     }]
   return (
     <nav className='fixed top-0 left-0 right-0 z-10 bg-white bg-opacity-100'>
-      <div className='flex justify-center md:justify-between items-center mx-auto py-2 px-4 md:px-40'>
+      <div className={` ${jetBrainsMono.className} flex justify-center md:justify-between items-center mx-8 md:mx-24 2xl:mx-56 py-6`}>
         <Link href='#hero-section'
-          className='text-2xl whitespace-nowrap text-[#e54971] font-semibold'>Saadia Shahid</Link>
+          className='text-3xl whitespace-nowrap text-[#e54971] '>Saadia Shahid</Link>
         <div className='menu hidden md:block md:w-auto'>
 
           {/* <ul>
@@ -29,7 +33,7 @@ const NavBar = () => {
               </li>
             ))}
           </ul> */}
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+          <ul className="flex text-xl p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
               <li key={index}>
                 <NavLink href={link.path} title={link.title}></NavLink>
