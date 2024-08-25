413e6a54bf9d4a9f4e5e0690b26e4605ef8cc3e0
DevSaadia <saadia.shahid@torontomu.ca>
2024-08-24 20:56:11 -0400

    made hero section reponsive

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -14,18 +14,24 @@ const HeroSection = () => {
             flex 
             items-center 
             '>
-            <div className='flex flex-col-reverse  md:flex-row items-center justify-center md:justify-between gap-8  md:gap-24 lg:gap-96 p-8 md:p-40 w-full'>
-                <div id='hero-content' className='md:flex-1 '>
+            <div className='flex flex-col-reverse  md:flex-row items-center justify-center md:justify-between gap-8 md:gap-4 lg:gap-10 xl:gap-16  m-8 md:m-8 2xl:m-56 w-full border border-purple-500'>
+                <div id='hero-content' className='md:flex-1 md:w-11/12 w-full border-red-700 border-4'>
+                    <p className='block md:hidden'>small</p>
+                    <p className='hidden md:block lg:hidden'>medium</p>
+                    <p className='hidden lg:block xl:hidden'>large</p>
+                    <p className='hidden xl:block 2xl:hidden'>xl</p>
+                    <p className='hidden 2xl:block'>2xl</p>
                     <h1
-                        className={`mb-7 md:pt-2 text-3xl md:text-5xl font-extrabold text-transparent md:w-11/12 w-2/3 bg-clip-text   bg-gradient-to-r from-[#e54971] to-[#e2a812] ${spaceMono.className}`}>&gt;
+                        className={`mb-7 md:pt-2 text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold whitespace-nowrap text-transparent w-full bg-clip-text   bg-gradient-to-r from-[#e54971] to-[#e2a812] ${spaceMono.className} border border-black`}>&gt;
                         <TypeAnimation
                             className='ml-6 '
+                            // sequence={["pwd"]}
                             sequence={["whoissaadia", 1000, "pwd", 1000, "ls", 1000, "echo \"hey\"", 1000]}
                             wrapper='span'
                             speed={20}
                             repeat={Infinity} />
                     </h1>
-                    <p className={`w-full md:w-96 ${averageSans.className} text-xl  mb-6`}>Driven by creativity, innovation and a desire to foster community among the minority groups in the tech industry.</p>
+                    <p className={`w-full xl:w-2/3 2xl:w-5/6 ${averageSans.className} text-xl lg:text-2xl mb-6 border border-black`}>Driven by creativity, innovation and a desire to foster community among the minority groups in the tech industry.</p>
                     <div id='buttons-container' className='flex flex-col md:flex-row gap-4'>
                         <button
                             className='
@@ -68,8 +74,8 @@ const HeroSection = () => {
 
                     </div>
                 </div>
-                <div id='hero-image' className='md:flex-1 flex justify-center md:justify-end'>
-                    <div className=' relative w-[400px] h-[400px] md:w-[300px] md:h-[300px] '>
+                <div id='hero-image' className='md:flex-1 flex justify-center md:justify-end border-red-700 border-4'>
+                    <div className=' relative w-[400px] h-[400px] md:w-[300px] md:h-[300px] lg:w-[500px] lg:h-[500px] '>
                         <Image
                             className='object-cover object-top rounded-2xl shadow-md w-full h-full'
                             src="/images/me.jpg"
