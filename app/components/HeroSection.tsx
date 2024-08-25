158fa2194395d3c330047d0a6eb644a80a6b72f1
DevSaadia <saadia.shahid@torontomu.ca>
2024-08-25 14:31:35 -0400

    code cleanup

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -16,7 +16,23 @@ const HeroSection = () => {
             flex 
             items-center 
             '>
-            <div className='flex flex-col-reverse  md:flex-row items-center justify-center md:justify-between gap-8 md:gap-4 lg:gap-10 xl:gap-16  m-8 md:m-24 2xl:m-56 w-full '>
+            <div
+                className='
+                    flex 
+                    flex-col-reverse  
+                    md:flex-row 
+                    items-center 
+                    justify-center 
+                    md:justify-between 
+                    gap-8 
+                    md:gap-4 
+                    lg:gap-10 
+                    xl:gap-16  
+                    m-8 
+                    md:m-24 
+                    2xl:m-56 
+                    w-full '
+            >
                 <div id='hero-content' className='md:flex-1 md:w-11/12 w-full'>
                     {/* <p className='hidden xs:block sm:hidden'>extra small</p>
                     <p className='block md:hidden'>small</p>
@@ -25,7 +41,23 @@ const HeroSection = () => {
                     <p className='hidden xl:block 2xl:hidden'>xl</p>
                     <p className='hidden 2xl:block'>2xl</p> */}
                     <h1
-                        className={`mb-7 md:pt-2 text-3xl  md:text-4xl lg:text-5xl xl:text-6xl font-extrabold whitespace-nowrap text-transparent w-auto inline-block  bg-clip-text   bg-gradient-to-r from-[#e54971] to-[#e2a812] ${spaceMono.className} `}>&gt;
+                        className={`
+                        mb-7 
+                        md:pt-2 
+                        text-3xl  
+                        md:text-4xl 
+                        lg:text-5xl 
+                        xl:text-6xl 
+                        font-extrabold 
+                        whitespace-nowrap 
+                        text-transparent
+                        w-auto inline-block  
+                        bg-clip-text   
+                        bg-gradient-to-r 
+                        from-[#e54971] 
+                        to-[#e2a812] 
+                        ${spaceMono.className} 
+                         `}>&gt;
                         <TypeAnimation
                             className='ml-6 '
                             sequence={["whoissaadia", 1000, "pwd", 1000, "ls", 1000, "echo \"hey\"", 1000]}
@@ -33,13 +65,24 @@ const HeroSection = () => {
                             speed={20}
                             repeat={Infinity} />
                     </h1>
-                    <p className={`w-full text-black xl:w-2/3 2xl:w-5/6 ${averageSans.className} text-xl lg:text-2xl 2xl:text-[32px] mb-6 `}>
+                    <p className={`
+                        w-full 
+                        text-black 
+                        xl:w-2/3 
+                        2xl:w-5/6 
+                        ${averageSans.className} 
+                        text-xl 
+                        lg:text-2xl 
+                        2xl:text-[32px]
+                        mb-6 `}>
                         Driven by creativity, innovation and a desire to foster community among the minority groups in the tech industry.</p>
-                    <div id='buttons-container' className='flex flex-col md:flex-row  gap-4'>
+                    <div
+                        id='buttons-container'
+                        className='flex flex-col md:flex-row  gap-4'>
                         <Button
                             startContent={<EnvelopeIcon className='w-6 h-6' />}
-                            radius='full' 
-                            size='lg' 
+                            radius='full'
+                            size='lg'
                             className='text-white bg-[#e54971] '
                             onClick={() => window.open("mailto:saadia.shahid@torontomu.ca?subject=Hello Saadia&body=Hi...", "_blank")}
                         >
@@ -55,49 +98,30 @@ const HeroSection = () => {
                         >
                             View Resume
                         </Button>
-                        {/* <button
-                            className='
-                                px-6
-                                py-2
-                                md:w-1/3
-                                md:auto
-                                rounded-full
-                                bg-[#e54971]
-                                transition
-                                duration-300
-                                hover:bg-[#b23554]
-                              text-white 
-                                mt-3'
-                            aria-label='Hire Me'
-                            onClick={() => window.open("mailto:saadia.shahid@torontomu.ca?subject=Hello Saadia&body=Hi...", "_blank")}
-                        >
-                            Hire Me
-                        </button>
-                        <button
-                            className='
-                             px-6
-                             py-2
-                             md:w-1/3
-                             md:auto
-                             rounded-full
-                             transition
-                             duration-300
-                             hover:bg-white
-                           text-[#e54971]
-                           ring-inset
-                           ring-4
-                           ring-[#e54971]
-                              mt-3'
-                            onClick={() => window.open("/files/Saadia_Shahid_Resume.pdf", "_blank")}
-                        >
-                            Download CV
-                        </button> */}
-
-
                     </div>
                 </div>
-                <div id='hero-image' className='md:flex-1 w-full h-full flex justify-center md:justify-end '>
-                    <div className=' relative w-full h-[300px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] xl:w-[500px] xl:h-[500px] '>
+                <div
+                    id='hero-image'
+                    className='
+                        md:flex-1 
+                        w-full 
+                        h-full 
+                        flex 
+                        justify-center 
+                        md:justify-end '
+                >
+                    <div
+                        className=' 
+                            relative 
+                            w-full 
+                            h-[300px] 
+                            md:w-[300px] 
+                            md:h-[300px] 
+                            lg:w-[400px] 
+                            lg:h-[400px] 
+                            xl:w-[500px] 
+                            xl:h-[500px] '
+                    >
                         <Image
                             className='object-cover object-top rounded-2xl shadow-md w-full h-full'
                             src="/images/me.jpg"
